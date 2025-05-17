4d847634db2a2ab8ac23f46d574dc6bb65784dee
jacobkhajjar <jacob.k.hajjar@gmail.com>
2025-05-17 13:01:07 +0100

    add asteroid sprites

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame # type: ignore
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,18 +12,29 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for entity in drawable:
+            entity.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
